b76fe0a2e55694f8ba56ef1d0405077ea927858b
Kishan <kishan@wappnet.com>
2026-03-19 12:52:23 +0530

    Created FullWidthTitleBannerImageSection - Specific menu page comment

---
+++ wp-content/themes/sage-angelathetton-theme/resources/views/blocks/full-width-title-banner-image-section.blade.php
@@ -0,0 +1,8 @@
+@if(!empty($responsiveCss))
+<style>{{ $responsiveCss }}</style>
+@endif
+<div id="{{ $blockId }}" class="full-width-title-banner-image-section" style="background-color: {{ $section_bg }};">
+  @if ($banner_image)
+    <img class="full-width-title-banner-img" src="{{ $banner_image['url'] }}" alt="{{ $banner_image['alt'] }}">
+  @endif
+</div>
