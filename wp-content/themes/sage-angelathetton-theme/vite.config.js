a9abf6371540be03b71b1553b4053082f2b8c812
Kishan <kishan@wappnet.com>
2026-04-14 17:30:17 +0530

    menu arrow issue

--- wp-content/themes/sage-angelathetton-theme/vite.config.js
+++ wp-content/themes/sage-angelathetton-theme/vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import { wordpressPlugin } from '@roots/vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import { wordpressPlugin, wordpressThemeJson } from '@roots/vite-plugin';
 
 export default defineConfig({
+  base: '/wp-content/themes/sage-angelathetton-theme/public/build/',
   plugins: [
     laravel({
       input: [
@@ -13,6 +14,35 @@ export default defineConfig({
       ],
       refresh: true,
     }),
+
     wordpressPlugin(),
+    // Generate the theme.json file in the public/build/assets directory
+    // based on the Tailwind config and the theme.json file from base theme folder
+    wordpressThemeJson({
+      disableTailwindColors: true,
+      disableTailwindFonts: true,
+      disableTailwindFontSizes: true,
+    }),
   ],
-});
+  resolve: {
+    alias: {
+      '@scripts': '/resources/js',
+      '@styles': '/resources/css',
+      '@fonts': '/resources/fonts',
+      '@images': '/resources/images',
+    },
+  },
+  // ✅ ADD THIS SECTION
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true, // hides bootstrap sass warnings
+      },
+    },
+  },
+  /*server: {
+    host: '192.168.10.147',
+    port: 5173,
+    cors: true,
+  },*/
+})
