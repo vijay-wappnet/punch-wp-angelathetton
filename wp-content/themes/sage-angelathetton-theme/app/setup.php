e734c2104a93c688f014dab0e182b6617a324943
Kishan <kishan@wappnet.com>
2026-04-22 18:07:10 +0530

    Remove default blocks

--- wp-content/themes/sage-angelathetton-theme/app/setup.php
+++ wp-content/themes/sage-angelathetton-theme/app/setup.php
@@ -171,7 +171,10 @@ add_action('wp_enqueue_scripts', function () {
 }, 5);
 
 
+
+
 require_once __DIR__ . '/custom-function.php';
+require_once __DIR__ . '/remove-default-blocks.php';
 require_once __DIR__ . '/blocks.php';
 require_once __DIR__ . '/woocommerce-customize-single-product.php';
 require_once __DIR__ . '/woocommerce-customize-cart.php';
